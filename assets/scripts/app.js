88a5df374ad6e745f33446e001cc1f1d9764b13d
Zarif Hossain <zarifhossain808@gmail.com>
2025-09-28 22:39:43 +1000

    learning string concatenation in JavaScript

---
+++ assets/scripts/app.js
@@ -0,0 +1,7 @@
+let currCalculatadResult = 0;
+
+currCalculatadResult = currCalculatadResult + 7;
+
+let calculationDescription = currCalculatadResult + " + 7";
+
+outputResult(currCalculatadResult, calculationDescription);
