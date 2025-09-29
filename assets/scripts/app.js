8323055c1c5fff2b2dc539ba0480aa04dcca1645
Zarif <zarif.hossain1909@gmail.com>
2025-09-29 19:01:07 +1000

    imported javascript code into html file, fixed intitial logic of app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,10 @@
-let currCalculatadResult = 0;
+const defaultResult = 0;
+let currCalculatadResult = defaultResult;
 
+// User input
 currCalculatadResult = currCalculatadResult + 7;
 
-let calculationDescription = currCalculatadResult + " + 7";
+// Calculation description
+let calculationDescription = defaultResult + " + 7";
 
 outputResult(currCalculatadResult, calculationDescription);
