102620637d0d0bf6bb591939efb352b355572bd7
Zarif <zarif.hossain1909@gmail.com>
2025-09-29 19:08:30 +1000

    applying template literals into calculationDescription String

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,6 +5,7 @@ let currCalculatadResult = defaultResult;
 currCalculatadResult = currCalculatadResult + 7;
 
 // Calculation description
-let calculationDescription = defaultResult + " + 7";
+// let calculationDescription = defaultResult + " + 7";
+let calculationDescription = `${defaultResult} + 7`;
 
 outputResult(currCalculatadResult, calculationDescription);
