86b83c8a55f5c7c1f8a082427d8d14449a18dff5
Zarif Hossain <zarifhossain808@gmail.com>
2025-10-01 22:32:05 +1000

    complete basic JavaScript assignment

---
+++ Assignment 1/assignment.js
@@ -0,0 +1,12 @@
+let userInput = 10;
+let result;
+
+result = userInput + 18;
+
+// the below 3 lines does not change the value of userInput
+result = result - 4;
+result = result / 6;
+result = result * 8;
+
+alert(`userInput: ${userInput}`);
+alert(`result: ${result}`);
