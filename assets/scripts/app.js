330791347f8c7d141fa371600a6ebd5323a4d9fe
Zarif Hossain <zarifhossain808@gmail.com>
2025-10-06 21:52:51 +1100

    created mathematical functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,25 @@
-const defaultResult = 0;
+const defaultResult = 100;
 let currCalculatadResult = defaultResult;
+
+function addition(num1, num2) {
+  const result = num1 + num2;
+  return result;
+}
+
+function substraction(num1, num2) {
+  const result = num1 - num2;
+  return result;
+}
+
+function multiply(num1, num2) {
+  const result = num1 * num2;
+  return result;
+}
+
+function divide(num1, num2) {
+  const result = num1 / num2;
+  return result;
+}
 
 // User input
 currCalculatadResult = currCalculatadResult + 7;
